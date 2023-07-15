12d5ee07d78c507254fd44718fa34b894ab18a70
jimao <jimao@jimaodeMBP.lan>
2023-07-15 19:23:07 +0800

    赋值input_offset

--- c/index.c
+++ c/index.c
@@ -23,6 +23,7 @@ int next_task = 0;
 int new_task(int indicator_index, int size, int input_offset) {
   task_list[next_task].indicator_index = indicator_index;
   task_list[next_task].size = size;
+  task_list[next_task].inputs_offset = input_offset;
   ti_indicator_info indicator = ti_indicators[indicator_index];
   task_list[next_task].inputs_buffer = malloc(sizeof(TI_REAL *) * indicator.inputs);
   task_list[next_task].inputs = malloc(sizeof(TI_REAL *) * indicator.inputs);
