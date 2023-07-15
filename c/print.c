28811a38867668b2c62b3c6035c4c782f5989b8c
jimao <jimao@jimaodeMBP.lan>
2023-07-16 00:30:55 +0800

    提交

---
+++ c/print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "candles.h"
+#include "indicators.h"
+
+int main() {
+  printf("你好，世界\n");
+  return 0;
+}
---
+++ script/cprint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc c/print.c -o dist/print
