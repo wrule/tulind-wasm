b76d1165faed183fe684c2d14db4ebe9fe1c7f0e
jimao <jimao@jimaodeMBP.lan>
2023-07-15 18:48:28 +0800

    结构体廷议

--- c/index.c
+++ c/index.c
@@ -6,40 +6,31 @@
 #define TASK_MAX 1000
 
 typedef struct {
-  int indicator;
+  int indicator_index;
   int size;
-  int inputs_length;
   TI_REAL * * inputs;
-  int options_length;
   TI_REAL * options;
-  int outputs_length;
   TI_REAL * * outputs;
 } Task;
 
 Task task_list[TASK_MAX];
 int next_task = 0;
 
-int new_task(
-  int indicator,
-  int size,
-  int inputs_length,
-  int options_length,
-  int outputs_length
-) {
-  task_list[next_task].indicator = indicator;
+int new_task(int indicator_index, int size) {
+  task_list[next_task].indicator_index = indicator_index;
   task_list[next_task].size = size;
 
-  task_list[next_task].inputs_length = inputs_length;
-  task_list[next_task].inputs = malloc(sizeof(TI_REAL *) * inputs_length);
-  for (int i = 0; i < inputs_length; ++i) {
+  ti_indicator_info indicator = ti_indicators[indicator_index];
+
+  task_list[next_task].inputs = malloc(sizeof(TI_REAL *) * indicator.inputs);
+  for (int i = 0; i < indicator.inputs; ++i) {
     task_list[next_task].inputs[i] = malloc(sizeof(TI_REAL) * size);
   }
 
-  task_list[next_task].options_length = options_length;
-  task_list[next_task].options = malloc(sizeof(TI_REAL) * options_length);
+  task_list[next_task].options = malloc(sizeof(TI_REAL) * indicator.options);
 
-  task_list[next_task].outputs = malloc(sizeof(TI_REAL *) * outputs_length);
-  for (int i = 0; i < outputs_length; ++i) {
+  task_list[next_task].outputs = malloc(sizeof(TI_REAL *) * indicator.outputs);
+  for (int i = 0; i < indicator.outputs; ++i) {
     task_list[next_task].outputs[i] = malloc(sizeof(TI_REAL) * size);
   }
 
@@ -73,6 +64,12 @@ TI_REAL outputs_number(
 
 void run_task(int task_index) {
   Task task = task_list[task_index];
-  ti_indicator_info indicator = ti_indicators[task.indicator];
+  ti_indicator_info indicator = ti_indicators[task.indicator_index];
   indicator.indicator(task.size, (const TI_REAL * *)task.inputs, task.options, task.outputs);
+}
+
+int main() {
+  printf("你好，世界\n");
+  printf("%d\n", ti_indicators[0].in)
+  return 0;
 }
--- script/cc.sh
+++ script/cc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-emcc c/index.c c/tiamalgamation.c -o dist/tulind.js \
-  -s MODULARIZE=1 \
-  -s EXPORT_NAME='Tulind' \
-  -s EXPORTED_FUNCTIONS='[
-    "_new_task",
-    "_inputs_number",
-    "_options_number",
-    "_outputs_number",
-    "_run_task"
-  ]'
+emcc c/index.c c/tiamalgamation.c -o dist/index.html
+  # -s MODULARIZE=1 \
+  # -s EXPORT_NAME='Tulind' \
+  # -s EXPORTED_FUNCTIONS='[
+  #   "_new_task",
+  #   "_inputs_number",
+  #   "_options_number",
+  #   "_outputs_number",
+  #   "_run_task"
+  # ]'
