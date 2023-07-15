7f5489c45d71a9872f0d0c553bb4ff0241c77779
jimao <jimao@jimaodeMBP.lan>
2023-07-16 00:45:48 +0800

    提交代码

--- c/print.c
+++ c/print.c
@@ -6,8 +6,11 @@
 int main() {
   printf("你好，世界\n");
   const ti_indicator_info *info = ti_indicators;
+  int first = 1;
   printf("[\n");
   while (info->name != 0) {
+    if (first == 0) printf(",\n");
+    first = 0;
     printf("\t{\n");
     printf("\t\t\"name\": \"%s\",\n", info->name);
     printf("\t\t\"full_name\": \"%s\",\n", info->full_name);
@@ -15,7 +18,10 @@ int main() {
     printf("\t\t\"inputs\": %d,\n", info->inputs);
     printf("\t\t\"options\": %d,\n", info->options);
     printf("\t\t\"outputs\": %d,\n", info->outputs);
-    printf("\t},\n");
+    printf("\t\t\"input_names\": [],\n");
+    printf("\t\t\"option_names\": [],\n");
+    printf("\t\t\"output_names\": []\n");
+    printf("\t}");
     ++info;
   }
   printf("]\n");
