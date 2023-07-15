609c4bfd88c09a9b39c3cd55050696a61cff678b
jimao <jimao@jimaodeMBP.lan>
2023-07-15 22:06:56 +0800

    编译命令

--- script/cc.sh
+++ script/cc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-emcc c/index.c c/tiamalgamation.c -o dist/index.html
-  # -s MODULARIZE=1 \
-  # -s EXPORT_NAME='Tulind' \
-  # -s EXPORTED_FUNCTIONS='[
-  #   "_new_task",
-  #   "_inputs_number",
-  #   "_options_number",
-  #   "_outputs_number",
-  #   "_run_task"
-  # ]'
+emcc c/index.c c/tiamalgamation.c -o dist/index.js \
+  -s MODULARIZE=1 \
+  -s EXPORT_NAME='Tulind' \
+  -s EXPORTED_FUNCTIONS='[
+    "_new_task",
+    "_free_task",
+    "_inputs_number",
+    "_options_number",
+    "_outputs_offset",
+    "_outputs_number",
+    "_run_task"
+  ]'
