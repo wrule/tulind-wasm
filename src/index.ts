49883700116efc6ab3a6c399a81b4f8149f25892
jimao <jimao@jimaodeMBP.lan>
2023-07-15 17:08:40 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ const Tulind = require('./tulind.js');
 async function main() {
   const tulind = await Tulind();
   const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const task_index = tulind._new_task(72, 10, 1, 1, 1);
-  tulind._inputs_number(0, 0, 0, 13);
-  console.log(tulind._outputs_number(0, 0, 0));
+  const task_index = tulind._new_task(72, source.length, 1, 1, 1);
+  source.forEach((number, x) => {
+    tulind._inputs_number(task_index, x, 0, number);
+  });
+  tulind._options_number(task_index, 0, 4);
+  tulind._run_task(task_index);
+  source.forEach((_, x) => {
+    console.log(tulind._outputs_number(task_index, x, 0));
+  });
 }
 
 main();
