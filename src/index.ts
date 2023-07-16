0730869c8d6963c6e2d635ba7bfc2a6269eccd01
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-16 21:12:40 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -28,11 +28,11 @@ interface Tulind_WASM {
   ) => void;
   _link_task: (task_index: number) => void,
   _run_task: (task_index: number) => void,
-  _run: (task_index: number) => void,
+  _run: () => void,
 }
 
 async function main() {
-  const tulind = await Tulind();
+  const tulind: Tulind_WASM = await Tulind();
   const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const task_index = tulind._new_task(72, source.length);
   source.forEach((number, offset) => {
