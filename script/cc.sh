d1eb52880c31d0c79858fda95325a8cdbe9bc02f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-17 14:48:17 +0800

    提交代码

--- script/cc.sh
+++ script/cc.sh
@@ -3,11 +3,18 @@ emcc -O3 c/index.c c/tiamalgamation.c -o dist/tulind.js \
   -s MODULARIZE=1 \
   -s EXPORT_NAME='Tulind' \
   -s EXPORTED_FUNCTIONS='[
+    "_free_task",
+    "_free_current",
+    "_reset",
+    "_init",
     "_new_task",
     "_inputs_number",
     "_inputs_map",
     "_options_number",
     "_outputs_number",
+    "_get_inputs_number",
+    "_inputs_offset",
+    "_outputs_offset",
     "_link_task",
     "_run_task",
     "_run"
