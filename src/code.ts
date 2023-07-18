864c24c7499b4844eb341dcf377efacfbd111243
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-18 21:08:02 +0800

    提交

--- src/code.ts
+++ src/code.ts
@@ -2,27 +2,14 @@ import { Indicator } from '.';
 
 export
 class Code {
-  public constructor(private readonly ind: Indicator) {
-    this.a();
-    // console.log(this.symbols);
-  }
+  public constructor(private readonly ind: Indicator) { }
 
   private get options() {
     return this.ind.option_names.length > 1;
   }
 
-  private get results() {
-    return this.ind.output_names.length > 1;
-  }
-
-  private ref_inputs: string[] = this.ind.input_names.slice();
-  private ref_options: string[] = this.ind.option_names
-    .map((name) => this.options ? `options.${name}` : name);
-  private ref_outputs: string[] = this.ind.output_names
-    .map((name) => this.results ? `results.${name}` : name);
-
-  private a() {
-
+  private option_name(name: string) {
+    return this.options ? `options.${name}` : name;
   }
 
   public Code() {
