a70e1fec63dc9e15c743b896f92c55bb7bf38927
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-18 21:34:32 +0800

    names

--- src/code.ts
+++ src/code.ts
@@ -12,6 +12,14 @@ class Code {
     return this.options ? `options.${name}` : name;
   }
 
+  private names(list: string[], type: 'number' | 'options' | '' = 'number') {
+    return list.map((name) => {
+      if (type === 'number') return `${name}: number`;
+      if (type === 'options') return `options.${name}`;
+      return name;
+    }).join(', ');
+  }
+
   private get argsCode() {
     const inputs = this.ind.input_names.map((name) => `${name}: number`).join(', ');
     const options = this.ind.options ?
